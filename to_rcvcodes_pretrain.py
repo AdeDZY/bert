684ec399b126b7c7632d9c20febf0dd39e67596f
zhuyund <zhuyund@cs.cmu.edu>
2019-03-28 17:58:50 -0400

    codes

--- to_rcvcodes_pretrain.py
+++ to_rcvcodes_pretrain.py
@@ -8,17 +8,42 @@ fout = open(sys.argv[2], 'w')
 for line in fin:
     json_dict = json.loads(line)
     codes = json_dict["codes"].values()
-    codes_tokens = [["RCV" + t for t in t2] for t2 in codes]
-    codes_text = " ".join([" ".join(t) for t in codes_tokens])
+
+    country_codes = codes.get("bip:countries:1.0", [])
+    topic_codes = codes.get("bip:topics:1.0", [])
+    industry_codes = codes.get("bip:industries:1.0", [])
+
+    country_code_text = "CODECOUNTRY " + '/ '.join(["RCV" + t for t in country_codes])
+    topic_code_text = "CODETOPIC " + '/ '.join(["RCV" + t for t in random.shuffle(topic_codes)])
+    industry_code_text = "CODEINDUSTRY " + '/ '.join(["RCV" + t for t in random.shuffle(industry_codes)])
+
     title = json_dict.get('headline', "")
-    if not title: title=""
+    if not title:
+        title = ""
     body = json_dict.get('body', "")
     doc = title + body
-    
-    sentence1 = codes_text 
     doc = doc.replace('\n', ' ')
-    sentence2 = ' '.join(doc.split(' ')[0:200])
-    sents = [sentence1, sentence2]
+    doc = ' '.join(doc.split(' ')[0:200])
+
+    sents = []
+
+    if industry_codes and not topic_codes:
+        sents.append(industry_codes)
+    if topic_codes and not industry_codes:
+        sents.append(topic_code_text)
+    elif topic_codes and industry_codes:
+        r = random.random()
+        if r < 0.4:
+            sents.append(topic_codes)
+        elif 0.4 <= r < 0.8:
+            sents.append(industry_codes)
+        elif 0.8 <= r:
+            sents.append(topic_codes)
+            sents.append(industry_codes)
+
+    if random.random() < 0.01 and country_codes:
+        sents.append(country_code_text)
+
     random.shuffle(sents)
     for sent in sents:
         fout.write(sent.strip())
